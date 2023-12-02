f8a0351fe2c6cc0c5161d1b9c3437030338f9042
Kayla Dale <kmeyers119@gmail.com>
2023-12-02 15:22:15 -0600

    alphabetized arrays

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let games = [
         runTime: 0.4
     },
     {
-        title: "Minecraft",
-        summary: "Minecraft is a game where players place blocks and go on adventures. This includes anything from crafting simple items like containers or weapons, to building structures like houses, castles, and cities, or even making complex mechanical-devices, all within the game's world.",
-        runTime: 90
+        title: "Counter-Strike: Global Offensive",
+        summary: "Two teams compete in multiple rounds of objective-based game modes with the goal of winning enough rounds to win the match.",
+        runTime: 1
     },
     {
         title: "Fortnite",
@@ -16,39 +16,14 @@ let games = [
         runTime: 62.5
     },
     {
-        title: "Grand Theft Auto V",
-        summary: "Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three protagonists—retired bank robber Michael De Santa, street gangster Franklin Clinton, and drug dealer and gunrunner Trevor Philips—and their attempts to commit heists while under pressure from a corrupt government agency and powerful criminals.",
-        runTime: 31.5
-    },
-    {
         title: "Forza Horizon 5",
         summary: "Forza Horizon 5 is a 2021 racing video game developed by Playground Games and published by Xbox Game Studios.",
         runTime: 20.5
     },
     {
-        title: "Super Smash Bros: Ultimate",
-        summary: "Super Smash Bros. Ultimate is a platform fighter for up to eight players in which characters from Nintendo games and other third-party franchises must try to knock each other out of an arena.",
-        runTime: 24
-    },
-    {
-        title: "Red Dead Redemption II",
-        summary: "After a robbery goes badly wrong in the western town of Blackwater, Arthur Morgan and the Van der Linde gang are forced to flee. With federal agents and the best bounty hunters in the nation massing on their heels, the gang must rob, steal and fight their way across the rugged heartland of America in order to survive.",
-        runTime: 50
-    },
-    {
-        title: "Overwatch",
-        summary: "Overwatch is a first-person multiplayer shooter, set in a future where a conflict between robots and humanity necessitated the creation of a task force, conveniently called 'Overwatch'",
-        runTime: 0.25
-    },
-    {
-        title: "Rocket League",
-        summary: "Rocket League is a multiplayer video game developed by Psyonix a video game developer based in California. It typically involves two teams battling against each other in a range of rounds that involve car-based basketball, football and hockey to score the most goals.",
-        runTime: 5.2
-    },
-    {
-        title: "Roblox",
-        summary: "Roblox is a platform that hosts more than 40 million user-generated experiences and lets users build their own from scratch. Within these experiences, users can play games, socialize with others, and earn and spend a virtual currency called Robux.",
-        runTime: 2.6
+        title: "Grand Theft Auto V",
+        summary: "Set within the fictional state of San Andreas, based on Southern California, the single-player story follows three protagonists—retired bank robber Michael De Santa, street gangster Franklin Clinton, and drug dealer and gunrunner Trevor Philips—and their attempts to commit heists while under pressure from a corrupt government agency and powerful criminals.",
+        runTime: 31.5
     },
     {
         title: "League of Legends",
@@ -56,14 +31,39 @@ let games = [
         runTime: 0.5
     },
     {
+        title: "Legend of Zelda: Tears of the Kingdom",
+        summary: "The player controls Link, who must help Princess Zelda to stop Ganondorf from destroying Hyrule.",
+        runTime: 57
+    },
+    {
+        title: "Minecraft",
+        summary: "Minecraft is a game where players place blocks and go on adventures. This includes anything from crafting simple items like containers or weapons, to building structures like houses, castles, and cities, or even making complex mechanical-devices, all within the game's world.",
+        runTime: 90
+    },
+    {
+        title: "Overwatch",
+        summary: "Overwatch is a first-person multiplayer shooter, set in a future where a conflict between robots and humanity necessitated the creation of a task force, conveniently called 'Overwatch'",
+        runTime: 0.25
+    },
+    {
         title: "PlayerUnknown's Battlegrounds",
         summary: "In the game, up to one hundred players parachute onto an island where they scavenge for weapons and equipment to take out other players while trying to be the last to survive.",
         runTime: 0.5
     },
     {
-        title: "Counter-Strike: Global Offensive",
-        summary: "Two teams compete in multiple rounds of objective-based game modes with the goal of winning enough rounds to win the match.",
-        runTime: 1
+        title: "Red Dead Redemption II",
+        summary: "After a robbery goes badly wrong in the western town of Blackwater, Arthur Morgan and the Van der Linde gang are forced to flee. With federal agents and the best bounty hunters in the nation massing on their heels, the gang must rob, steal and fight their way across the rugged heartland of America in order to survive.",
+        runTime: 50
+    },
+    {
+        title: "Roblox",
+        summary: "Roblox is a platform that hosts more than 40 million user-generated experiences and lets users build their own from scratch. Within these experiences, users can play games, socialize with others, and earn and spend a virtual currency called Robux.",
+        runTime: 2.6
+    },
+    {
+        title: "Rocket League",
+        summary: "Rocket League is a multiplayer video game developed by Psyonix a video game developer based in California. It typically involves two teams battling against each other in a range of rounds that involve car-based basketball, football and hockey to score the most goals.",
+        runTime: 5.2
     },
     {
         title: "Super Mario Odyssey",
@@ -71,9 +71,9 @@ let games = [
         runTime: 12.5
     },
     {
-        title: "Legend of Zelda: Tears of the Kingdom",
-        summary: "The player controls Link, who must help Princess Zelda to stop Ganondorf from destroying Hyrule.",
-        runTime: 57
+        title: "Super Smash Bros: Ultimate",
+        summary: "Super Smash Bros. Ultimate is a platform fighter for up to eight players in which characters from Nintendo games and other third-party franchises must try to knock each other out of an arena.",
+        runTime: 24
     },
 ];
 
@@ -83,12 +83,13 @@ function loadNames() {
   
     for (let game in games) {
         let li = document.createElement('li');  
-       li.innerHTML = game;
+       li.innerHTML = games[game].title;
         ul.appendChild(li);
     }
     
  }
 
+ 
 
 
 function clicked(event) {
